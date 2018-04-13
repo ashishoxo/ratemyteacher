a90b768cb0474b851dee6e7c83990d18f871747b
rahul <rahul@github>
2018-04-13 16:15:50 +0530

    welcome page 1and schema

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,38 +102,7 @@
     <div class="main-search-inner">
 
         <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2>Find Nearby Attractions</h2>
-                    <h4>Expolore top-rated attractions, activities and more</h4>
-
-                    <div class="main-search-input">
-
-                        <div class="main-search-input-item">
-                            <input type="text" placeholder="What are you looking for?" value=""/>
-                        </div>
-
-                        <div class="main-search-input-item location">
-                            <input type="text" placeholder="Location" value=""/>
-                            <a href="#"><i class="fa fa-dot-circle-o"></i></a>
-                        </div>
-
-                        <div class="main-search-input-item">
-                            <select data-placeholder="All Categories" class="chosen-select" >
-                                <option>All Categories</option> 
-                                <option>Shops</option>
-                                <option>Hotels</option>
-                                <option>Restaurants</option>
-                                <option>Fitness</option>
-                                <option>Events</option>
-                            </select>
-                        </div>
-
-                        <button class="button" onclick="window.location.href='listings-half-screen-map-list.html'">Search</button>
-
-                    </div>
-                </div>
-            </div>
+            
         </div>
 
     </div>
