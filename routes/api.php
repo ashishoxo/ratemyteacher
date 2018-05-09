ef93b369f00e687fb334a5de67bd208ada94a94e
ashishoxo <ashish9436@gmail.com>
2018-05-10 03:03:50 +0530

    before work commit

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['prefix' => 'api/v1', 'middleware' => 'auth:api'], function () {
+	Route::post('/register-user', 'RegisterController@register');
+});
